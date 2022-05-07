05cc8244af3f11152de9287df40a4297a9a4d6d6
yenuri <yenuri.yz.28@gmail.com>
2022-05-06 21:52:00 -0500

    useState and useEffect

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,10 +1,23 @@
 import { Box, Button, TextField } from '@mui/material'
 import { YoutubeItem } from '../types/youtubeItem'
+import { useEffect, useState } from 'react'
 
-const handleSubmit = (entry: YoutubeItem) => {
-    console.log(entry)
-}
+const blackList = ['demo', 'test']
 const Editor = () => {
+    const [videoName, setVideoName] = useState('')
+    const [videoUrl, setVideoUrl] = useState('')
+
+    const handleSubmit = () => {
+        console.log(videoName, videoUrl)
+    }
+
+    useEffect(() => {
+        if (!videoName) return
+        if (blackList.some((e) => videoName == e)) {
+            setVideoName('')
+        }
+    }, [videoName])
+
     return (
         <Box
             component="form"
@@ -21,6 +34,8 @@ const Editor = () => {
                     id="video-name"
                     label="video name"
                     variant="outlined"
+                    value={videoName}
+                    onChange={(e) => setVideoName(e.target.value)}
                 />
             </div>
             <div>
@@ -28,23 +43,13 @@ const Editor = () => {
                     id="video-url"
                     label="video url"
                     variant="outlined"
+                    value={videoUrl}
+                    onChange={(e) => setVideoUrl(e.target.value)}
                 />
             </div>
             <div>
-                <Button
-                    variant="contained"
-                    onClick={() =>
-                        handleSubmit({
-                            videoName: (
-                                document.getElementById('video-name') as any
-                            ).value,
-                            videoUrl: (
-                                document.getElementById('video-url') as any
-                            ).value,
-                        })
-                    }
-                >
-                    Create Entry 
+                <Button variant="contained" onClick={handleSubmit}>
+                    Create Entry
                 </Button>
             </div>
         </Box>
